e6bccf1feee0799c567b3cec6951caf32fdcc431
Jão Xavier <jvxavier.lima67@gmail.com>
2022-12-29 16:00:55 -0300

    add details to entity monster, command line runner and category created

--- src/main/java/io/github/jaoxavier/monstershop/MonsterShopApplication.java
+++ src/main/java/io/github/jaoxavier/monstershop/MonsterShopApplication.java
@@ -1,10 +1,20 @@
 package io.github.jaoxavier.monstershop;
 
+import io.github.jaoxavier.monstershop.domain.entity.Monster;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MonsterShopApplication {
+	@Bean
+	public CommandLineRunner init(@Autowired Monster monster){
+		return args -> {
+				Monster monster1 = new Monster();
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonsterShopApplication.class, args);
---
+++ src/main/java/io/github/jaoxavier/monstershop/domain/enums/Category.java
@@ -0,0 +1,8 @@
+package io.github.jaoxavier.monstershop.domain.enums;
+
+public enum Category {
+    MONSTER_ENERGY,
+    MONSTER_ULTRA,
+    JUICE_MONSTER,
+    DRAGON_TEA
+}
